c65e8c56b23fa5242a54c89cfae52ab307fa75c1
STharshan <sivatharshan81@gmail.com>
2025-07-28 11:49:51 +0530

    letter mistake

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
-import Cambelt from "./pages/cambelt"
+import Cambelt from "./pages/Cambelt"
 import Mot from "./pages/Mot"
 import Contact from "./pages/contact"
 import Diagnostics from "./pages/diagnostics"
